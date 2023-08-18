ed6a2837fb58bb80d2ec18dd6b2e8435997d0387
SAMIVo007 <ys142051@gmail.com>
2023-08-18 12:11:22 +0530

    first-commit
    v

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/ui/theme/Type.kt
@@ -0,0 +1,34 @@
+package com.example.diceroller.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.diceroller.R
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    )
+)
+
+val gow = FontFamily(
+    Font(R.font.godofwar,FontWeight.Normal)
+)
+
+//val godofwar = Typography(
+//    bodyLarge = TextStyle(
+//        fontFamily = gow,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//)
